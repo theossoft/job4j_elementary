0f0e2ebd78214441e85ef359a9c2e38f5bb5cc77
theossoft <theossoft@gmail.com>
2020-10-22 23:58:13 +0300

    3.3. Разбор первой программы. [#394227]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Sergey Ivanov");
+        System.out.println("26.01.1983");
+    }
+}
