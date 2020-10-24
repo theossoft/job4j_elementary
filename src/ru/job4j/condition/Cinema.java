6599f305a030aade5207597acc49cdb98a82e2fe
theossoft <theossoft@gmail.com>
2020-10-24 23:09:16 +0300

    4.3.1. Оператор && [#394160]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -10,9 +10,20 @@ public class Cinema {
         }
     }
 
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
     public static void main(String[] args) {
         Cinema.access(21);
         Cinema.access(16);
-
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
     }
 }
