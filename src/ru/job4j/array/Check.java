f35509919870660f274efe3c8ee2ceb5cad2e9c2
theossoft <theossoft@gmail.com>
2020-10-28 23:09:23 +0300

    6.3. Массив заполнен true или false [#394177]

---
+++ src/ru/job4j/array/Check.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class Check {
+    public static boolean mono(boolean[] data) {
+        boolean result = true;
+        boolean temp = data[0];
+        for (int i = 0; i < data.length; i++) {
+            if (data[i] == temp) {
+                temp = data[i];
+            } else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+}
--- test/ru/job4j/array/FindLoopTest.java
+++ test/ru/job4j/array/FindLoopTest.java
@@ -8,7 +8,7 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas5Then0() {
-        int input[] = {5, 4, 3, 2};
+        int[] input = {5, 4, 3, 2};
         int value = 5;
         int result = FindLoop.indexOf(input, value);
         int expect = 0;
@@ -17,7 +17,7 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas6ThenNothing() {
-        int input[] = {5, 4, 3, 2};
+        int[] input = {5, 4, 3, 2};
         int value = 6;
         int result = FindLoop.indexOf(input, value);
         int expect = -1;
