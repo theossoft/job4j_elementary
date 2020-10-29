c4d016dc6b478f5dca3356497366cf619811c8c0
theossoft <theossoft@gmail.com>
2020-10-30 00:27:37 +0300

    6.7.4. Выигрышные комбинации в сокобан [#394178]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,15 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (monoVertical(board, i) || monoHorizontal(board, i)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
