404511f5fad551564158c1937796559cca4d2b2c
Theos <theossoft@gmail.com>
2022-08-28 22:13:48 +0300

    1. Модульные тесты. [#159435]

--- src/test/java/ru/job4j/calculator/ConverterTest.java
+++ src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -1,5 +1,6 @@
 package ru.job4j.calculator;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -7,10 +8,29 @@ import static org.junit.Assert.*;
 public class ConverterTest {
 
     @Test
-    public void rubleToEuro() {
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
     }
 
     @Test
-    public void rubleToDollar() {
+    public void whenConvert180RblThen3Dlr() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert0RblThen0Dlr() {
+        float in = 0;
+        float expected = 0;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
     }
 }
