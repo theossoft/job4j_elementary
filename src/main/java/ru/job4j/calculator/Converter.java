37c649da5720a78d4331e4f7ce5f75dc45872657
Theos <theossoft@gmail.com>
2022-08-28 12:53:12 +0300

    0. Что такое тестирование [#159431 #337106]

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -15,17 +15,18 @@ public class Converter {
         float euro = Converter.rubleToEuro(140);
         System.out.println("140 rubles are " + euro + " euro.");
 
-        float dollar = Converter.rubleToDollar(140);
+        float dollar = Converter.rubleToDollar(180);
         System.out.println("140 rubles are " + dollar + " dollar.");
 
-        float in = 140;
+        float inEuro = 140;
+        float inDollar = 180;
         float expectedEuro = 2;
-        float expectedDollar = 2.3333333f;
-        float outEuro = Converter.rubleToEuro(in);
-        float outDollar = Converter.rubleToDollar(in);
+        float expectedDollar = 3;
+        float outEuro = Converter.rubleToEuro(inEuro);
+        float outDollar = Converter.rubleToDollar(inDollar);
         boolean passedEuro = expectedEuro == outEuro;
         boolean passedDollar = expectedDollar == outDollar;
         System.out.println("140 rubles are 2. Test result : " + passedEuro);
-        System.out.println("140 rubles are 2.3333333. Test result : " + passedDollar);
+        System.out.println("180 rubles are 3. Test result : " + passedDollar);
     }
 }
---
+++ src/test/java/ru/job4j/calculator/ConverterTest.java
@@ -0,0 +1,16 @@
+package ru.job4j.calculator;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void rubleToEuro() {
+    }
+
+    @Test
+    public void rubleToDollar() {
+    }
+}
