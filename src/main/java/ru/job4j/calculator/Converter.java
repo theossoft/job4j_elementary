eb861454da90287d0bdd0054f2ea9d7cbe87d7ad
Theos <theossoft@gmail.com>
2022-08-28 11:06:16 +0300

    9. Площадь треугольника. [#175474 #336813]

--- src/main/java/ru/job4j/calculator/Converter.java
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -17,5 +17,15 @@ public class Converter {
 
         float dollar = Converter.rubleToDollar(140);
         System.out.println("140 rubles are " + dollar + " dollar.");
+
+        float in = 140;
+        float expectedEuro = 2;
+        float expectedDollar = 2.3333333F;
+        float outEuro = Converter.rubleToEuro(in);
+        float outDollar = Converter.rubleToDollar(in);
+        boolean passedEuro = expectedEuro == outEuro;
+        boolean passedDollar = expectedDollar == outDollar;
+        System.out.println("140 rubles are 2. Test result : " + passedEuro);
+        System.out.println("140 rubles are 2.3333333. Test result : " + passedDollar);
     }
 }
